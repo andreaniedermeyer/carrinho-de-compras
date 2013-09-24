d30a9d5db4576698c0ae947158a6607e88a07ede
31200222 <31200222@090012-P33S13.macklabs.br>
2013-09-24 20:56:44 -0300

    Classe de Teste da classe Produto

---
+++ test/br/calebe/exemplos/ex01/ProdutoTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calebe.exemplos.ex01;
+
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ *
+ * @author 31200222
+ */
+public class ProdutoTest {
+   
+    
+    @Test
+    public void criarProduto(){
+        String nome = "Enciclopedia";
+        Double preco = 150.99;
+        Produto prod = new Produto(nome, preco);
+        Assert.assertEquals(preco, prod.getPreco(),0d);
+    }
+    
+    @Test
+    public void verificarProduto(){
+        Produto prod1 = new Produto("Enciclopedia",150.99);
+        Produto prod2 = new Produto("Enciclopedia",150.99);
+        Assert.assertEquals(prod1, prod2);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void nomeVazio(){
+        Produto prod1 = new Produto("", 150.99);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void precoInvalido(){
+        Produto prod1 = new Produto("Enciclopedia", -150.00);
+    }
+}
